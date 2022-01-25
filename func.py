5edc222a0a43e18685dd54fbbc1a8273acd6ef19
SindhuSravyaMangalapalli <92310290+SindhuSravyaMangalapalli@users.noreply.github.com>
2022-01-25 22:34:52 +0530

    Create func.py

---
+++ func.py
@@ -0,0 +1,11 @@
+def fun()
+fun()=most_frequent
+s = 'missisippi' 
+s_list=list(s) 
+print(s_list) 
+#Sorting in descending order: 
+d_sort=sorted(s_list) 
+d_str=''.join(d_sort) 
+print(d_str) 
+ 
+  
